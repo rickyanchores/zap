c99a9ee4bbd9f38e5b320b9dede3c0d552fccb8f
RickyAnchores <ricky23anchores@gmail.com>
2022-11-26 22:21:13 +0000

    added card

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Card from '../../Components/Card/Card'
 
 const Home = () => {
   return (
-    <div className='Home'>Home</div>
+    <div className='Home'>
+      <h1>Home</h1>
+      <div className="container">
+        <Card />
+      </div>
+    </div>
   )
 }
 
